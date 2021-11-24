ddf307afc5bfbc2c541447c5c2c72e570971e6d7
rock <383329495@qq.com>
2021-11-24 09:54:04 +0800

    update kv

--- interface/cli/kv/kv.go
+++ interface/cli/kv/kv.go
@@ -160,11 +160,6 @@ func main() {
 	if err != nil {
 		logger.Fatal(err)
 	}
-	//开始订阅
-	netSubs, err := topic.Subscribe()
-	if err != nil {
-		logger.Fatal(err)
-	}
 
 	// Use a special pubsub topic to avoid disconnecting
 	// from globaldb peers.
@@ -174,6 +169,12 @@ func main() {
 	//死循环监听订阅
 	go func() {
 		for {
+			//开始订阅
+			netSubs, err := topic.Subscribe()
+			if err != nil {
+				logger.Fatal(err)
+			}
+
 			msg, err := netSubs.Next(ctx)
 			if err != nil {
 				fmt.Println(err)
