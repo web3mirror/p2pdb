58b8d83d7848a61c0626f9881df5f8b2d5c4e821
rock <383329495@qq.com>
2021-12-10 02:48:44 +0800

    update test.php

--- test/test.php
+++ test/test.php
@@ -2,45 +2,52 @@
 <?php
 class Message{
     public $id;     //事件唯一id， 发送一次递增一次
-    public $time;   //系统毫秒时间戳
+    public $timestamp;   //系统毫秒时间戳
     public $msg;    //发生消息内容
     public $nodeId; //节点id
     public $hostId; //主机id
     public $lastId; //因果关系事件id,为0代表没有因果关系
+    public $type; //事件type sending or receiving
+    public $receivingId; //事件接收本地自增id
 }
 
 
 
-
-
-//事件没有因果关系时，比较系统时钟
+/**
+ * 比较逻辑时钟,返回后发生的事件id
+ */
 function compareClock(array $a, array $b){
-    $dist = $a['time'] - $b['time'];
-
-    if($dist==0) {
-        $dist = compareId((int)$a['id'], (int)$b['id']); 
+    /*
+     *同一个节点发出的,事件具有因果关系
+     *先发出的在前，比较递增唯一id
+    */
+    if($a['nodeId']==$b['nodeId']){
+         $maxId=getMaxId($a['id'],$b['id']);
+         return $maxId;
     }
 
-    return $dist;
-}
+    /*
+     *不同节点发出的消息，timestamp相等表示出现并发,按照指定规则收敛
+     *消息发出的比接收的大
+     */
+    if($a['timestamp']==$b['timestamp']){
 
-//系统时钟一致时,比较事件id
-function compareId(int $aId, int $bId) {
-    //b事件在前   b>a
-    $dist=$aId-$bId;
-   if(($aId-$bId)>0){
-       return $dist;
-   }
+        if($a['type']=="sending" && $b['timestamp']=="receiving"){
+            $a['timestamp'] =  $a['timestamp']+1;      
+        }
 
-    //a事件在前  a>b
-    $dist=$bId-$aId;
-    if(($bId-$aId)>0){
-        return $dist;
+        if($a['type']=="receiving" && $b['timestamp']=="sending"){
+            $b['timestamp'] =  $b['timestamp']+1;      
+        }
+
+        $maxId=getMaxIdByTimestamp($a['timestamp'],$b['timestamp'],$a['id'],$b['id']);
+        return $maxId;
     }
-    //无法判断谁在前，last win 或者frist win 
-    return $aId;
+
+    /**
+     * 不同节点，都是发送 或者都是接收的请求
+     * 按照last win 收敛
+     */
+    $maxId=getMaxIdByRid($a['receivingId'],$b['receivingId'],$a['id'],$b['id']);
+    return $maxId;
 }
-
-
-
-$a
